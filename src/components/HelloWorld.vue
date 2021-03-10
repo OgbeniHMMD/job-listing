524e984b9dacb801170fe96ec2f7efb23ce0cb21
OgbeniHMMD <olajide.a.hammed@gmail.com>
2021-03-10 05:25:14 +0100

    Installed TailwindCss

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,11 +2,14 @@
   <h1>{{ msg }}</h1>
 
   <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">Vite Documentation</a> |
+    <a href="https://vitejs.dev/guide/features.html" target="_blank"
+      >Vite Documentation</a
+    >
+    |
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
   </p>
 
-  <button @click="state.count++">count is: {{ state.count }}</button>
+  <button @click="inc">count is: {{ state.counts.x }}</button>
   <p>
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
@@ -14,13 +17,16 @@
 </template>
 
 <script setup>
-import { defineProps, reactive } from 'vue'
+import { defineProps, reactive, ref } from "vue"
 
 defineProps({
-  msg: String
+  msg: String,
 })
 
-const state = reactive({ count: 0 })
+const state = reactive({ count: { x: 19 }, x: 9 })
+
+const x = ref(7)
+const inc = () => state.count.x++
 </script>
 
 <style scoped>
