da08a605e8dca3806fbcb31e3a85e895d3ccca22
OgbeniHMMD <olajide.a.hammed@gmail.com>
2021-03-10 06:42:40 +0100

    Basic color setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
 module.exports = {
   purge: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   darkMode: false, // or 'media' or 'class'
+
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "1em",
+    },
+    extend: {
+      colors: {
+        primary: "#0F4A7B",
+        secondary: "#F9FBFD",
+        accent: "#F57D7D",
+      },
+    },
   },
+
   variants: {
     extend: {},
   },
+
   plugins: [],
 }
