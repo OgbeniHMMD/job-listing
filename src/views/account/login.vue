0a0b83603408445f8134c73da6fcacee225f3b69
OgbeniHMMD <olajide.a.hammed@gmail.com>
2021-03-10 07:36:42 +0100

    Added slang to login page

--- src/views/account/login.vue
+++ src/views/account/login.vue
@@ -1,3 +1,22 @@
 <template>
-  <main>LOGIN</main>
+  <main class="min-h-screen flex">
+    <div
+      class="w-1/2 bg-accent"
+      style="clip-path: polygon(0 0, 100% 0, 88% 100%, 0 100%)"
+    >
+      <div
+        class="min-h-screen bg-primary text-white flex flex-col justify-center p-8"
+        style="clip-path: polygon(0 0, 93% 0, 86% 100%, 0% 100%)"
+      >
+        <div class="text-bolder text-3xl pr-3">
+          Find the best candidates for your organisation.
+        </div>
+        <div>
+          <img alt="Vue logo" src="/images/login-bg.svg" />
+        </div>
+      </div>
+    </div>
+
+    <div class="w-1/2 flex flex-col justify-center items-center">FORM</div>
+  </main>
 </template>
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,11 +7,17 @@ module.exports = {
       center: true,
       padding: "1em",
     },
+
     extend: {
       colors: {
         primary: "#0F4A7B",
         secondary: "#F9FBFD",
         accent: "#F57D7D",
+      },
+
+      fontFamily: {
+        display: ["Gilroy"],
+        body: ["Gilroy"],
       },
     },
   },
